9108ebf7b9387fdcb97350dbc853a27e17de22d1
hyoti33333 <93581941+hyoti33333@users.noreply.github.com>
2025-10-27 14:04:31 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,7 @@
+def solution(numbers, direction):
+    if direction == 'right':
+        answer = numbers[-1:] + numbers[:-1]
+    else:
+        answer = numbers[1:] + numbers[:1]
+        
+    return answer
