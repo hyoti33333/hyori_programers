778212918aa0130741b43a5f10ff521739709841
hyoti33333 <93581941+hyoti33333@users.noreply.github.com>
2025-10-28 10:10:20 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,7 +1,3 @@
 def solution(my_string):
-    for ms in my_string:
-        if ms.isalpha():
-            my_string = my_string.replace(ms,' ')
-    my_string = my_string.split()
-    
-    return sum(list(map(int,my_string)))
+    arr = ''.join([ ms if ms.isdecimal() else ' ' for ms in my_string ]).split()
+    return  sum(list(map(int,arr)))
