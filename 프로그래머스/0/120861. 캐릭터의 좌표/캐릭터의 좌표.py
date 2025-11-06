48a481fa21263187a6bb2f2590ded4005bccd0d5
hyoti33333 <93581941+hyoti33333@users.noreply.github.com>
2025-11-06 17:27:26 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,27 @@
+def solution(keyinput, board):
+    cod = [board[0] // 2, board[1] // 2]
+    start = [0, 0]
+
+    for i in keyinput:
+        if i == 'up':
+            start[1] += 1
+        elif i == 'down':
+            start[1] -= 1
+        elif i == 'left':
+            start[0] -= 1
+        elif i == 'right':
+            start[0] += 1
+
+        # x축 경계 처리
+        if start[0] > cod[0]:
+            start[0] = cod[0]
+        elif start[0] < -cod[0]:
+            start[0] = -cod[0]
+
+
+        if start[1] > cod[1]:
+            start[1] = cod[1]
+        elif start[1] < -cod[1]:
+            start[1] = -cod[1]
+
+    return start
